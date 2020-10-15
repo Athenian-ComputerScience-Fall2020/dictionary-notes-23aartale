465b985c40059eea431c652f53be6aef6f33f25a
23aartale <23aartale@athenian.org>
2020-10-15 17:12:34 +0000

    notes

--- my_code.py
+++ my_code.py
@@ -1,2 +1,6 @@
 # Use this to take notes on the Edpuzzle video. Try each example rather than just watching it - you will get much more out of it!
 #  
+student = {'name': 'John', 'age': 25, 'courses': ['math', 'CompSci']}
+
+for key, value in student.items():
+    print(key, value)
